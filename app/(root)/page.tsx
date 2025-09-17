75a1d58a3260d61e3c226f538e7bfc69bc0fa631
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-17 14:03:14 -0300

    Add clerk dark theme and RootLayout component structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
-"use client"
-
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <main>
-      <UserButton />
-    </main>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
