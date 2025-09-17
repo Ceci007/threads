bf39e6c9fb930bec1bf104e1accf971077ed5541
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-17 14:12:21 -0300

    Add all assets on public folder

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const Topbar = () => {
+const Topbar = () => { 
   return (
-    <div>
-      <h1>Topbar</h1>
-    </div>
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="Logo" width={28} height={28} />
+      </Link>
+    </nav>
   )
 }
 
