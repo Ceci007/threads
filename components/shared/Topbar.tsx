00e030210349b48927d4198c38245920c7461f8d
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-17 14:38:29 -0300

    Add Topbar component

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
   
 const Topbar = () => { 
   return (
@@ -9,6 +10,24 @@ const Topbar = () => {
         <Image src="/logo.svg" alt="Logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image src="/assets/logout.svg" alt="Logout icon" width={24} height={24} />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher 
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4"
+            }
+          }}
+        />
+      </div>
     </nav>
   )
 }
